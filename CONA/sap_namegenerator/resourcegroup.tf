6f5eda9a741dd9ab20d8e33ed7354682e81a11d2
Kimmo Forss <kimmo.forss@microsoft.com>
2020-12-16 23:49:54 +0200

    Name updates

--- CONA/sap_namegenerator/resourcegroup.tf
+++ CONA/sap_namegenerator/resourcegroup.tf
@@ -1,13 +1,13 @@
 locals {
 
   sdu_name = length(var.codename) > 0 ? (
-    upper(format("%s-%s-%s-%s", local.env_verified, local.location_short, var.codename, var.sap_sid))) : (
-    upper(format("%s-%s-%s", local.env_verified, local.location_short, var.sap_sid))
+    upper(format("RG_%s-%s-%s-%s", local.env_verified, local.location_short, var.codename, var.sap_sid))) : (
+    upper(format("RG_%s-%s-%s", local.env_verified, local.location_short, var.sap_sid))
   )
 
-  deployer_name  = upper(format("%s-%s-%s", local.deployer_env_verified, local.deployer_location_short, local.dep_vnet_verified))
-  landscape_name = upper(format("%s-%s-%s", local.landscape_env_verified, local.location_short, local.sap_vnet_verified))
-  library_name   = upper(format("%s-%s", local.library_env_verified, local.location_short))
+  deployer_name  = upper(format("RG_%s-%s", local.deployer_env_verified, local.deployer_location_short))
+  landscape_name = upper(format("RG_%s-%s", local.landscape_env_verified, local.location_short))
+  library_name   = upper(format("RG_%s-%s", local.library_env_verified, local.location_short))
 
   // Storage account names must be between 3 and 24 characters in length and use numbers and lower-case letters only. The name must be unique.
   deployer_storageaccount_name       = substr(replace(lower(format("%s%s%sdiag%s", local.deployer_env_verified, local.deployer_location_short, local.dep_vnet_verified, local.random_id_verified)), "/[^a-z0-9]/", ""), 0, var.azlimits.stgaccnt)
