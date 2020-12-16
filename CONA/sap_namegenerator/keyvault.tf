e650ce4c2821e6d3f7e675b2a39ee4e34a182a1f
Kimmo Forss <kimmo.forss@microsoft.com>
2020-12-16 23:57:39 +0200

    fixes

--- CONA/sap_namegenerator/keyvault.tf
+++ CONA/sap_namegenerator/keyvault.tf
@@ -6,8 +6,8 @@ locals {
   deployer_private_keyvault_name = format("%s%sprvt%s", replace(local.deployer_env_verified, "/[^A-Za-z0-9]/", ""), local.deployer_location_short, local.random_id_verified)
   deployer_user_keyvault_name    = format("%s%suser%s", replace(local.deployer_env_verified, "/[^A-Za-z0-9]/", ""), local.deployer_location_short, local.random_id_verified)
 
-  landscape_private_keyvault_name = format("%s%s%sprvt%s", replace(local.landscape_env_verified, "/[^A-Za-z0-9]/", ""), local.location_short, local.random_id_verified)
-  landscape_user_keyvault_name    = format("%s%s%suser%s", replace(local.landscape_env_verified, "/[^A-Za-z0-9]/", ""), local.location_short, local.random_id_verified)
+  landscape_private_keyvault_name = format("%s%sprvt%s", replace(local.landscape_env_verified, "/[^A-Za-z0-9]/", ""), local.location_short, local.random_id_verified)
+  landscape_user_keyvault_name    = format("%s%suser%s", replace(local.landscape_env_verified, "/[^A-Za-z0-9]/", ""), local.location_short, local.random_id_verified)
 
   library_private_keyvault_name = format("%s%sSAPLIBprvt%s", replace(local.library_env_verified, "/[^A-Za-z0-9]/", ""), local.location_short, local.random_id_verified)
   library_user_keyvault_name    = format("%s%sSAPLIBuser%s", replace(local.library_env_verified, "/[^A-Za-z0-9]/", ""), local.location_short, local.random_id_verified)
